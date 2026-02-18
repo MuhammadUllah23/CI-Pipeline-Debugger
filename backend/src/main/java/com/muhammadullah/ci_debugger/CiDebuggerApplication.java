044e43feb902d5db51d73bb79c5c90319be7c02b
M.Ullah <muhammadhullah23@gmail.com>
2026-02-18 16:57:17 -0500

    setup

---
+++ backend/src/main/java/com/muhammadullah/ci_debugger/CiDebuggerApplication.java
@@ -0,0 +1,13 @@
+package com.muhammadullah.ci_debugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDebuggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiDebuggerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/muhammadullah/ci_debugger/CiDebuggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammadullah.ci_debugger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiDebuggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
