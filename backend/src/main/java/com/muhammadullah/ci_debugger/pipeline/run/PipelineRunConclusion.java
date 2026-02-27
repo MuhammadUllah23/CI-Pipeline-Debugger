280b738dbad305bfd7d2347a194219045716604b
M.Ullah <muhammadhullah23@gmail.com>
2026-02-27 01:26:58 -0500

    add PipelineRunConclusion Enums

---
+++ backend/src/main/java/com/muhammadullah/ci_debugger/pipeline/run/PipelineRunConclusion.java
@@ -0,0 +1,13 @@
+package com.muhammadullah.ci_debugger.pipeline.run;
+
+public enum PipelineRunConclusion {
+    SUCCESS,
+    FAILURE,
+    CANCELLED,
+    SKIPPED,
+    TIMED_OUT,
+    NEUTRAL,
+    ACTION_REQUIRED,
+    STALE,      // optional but useful
+    UNKNOWN
+}
