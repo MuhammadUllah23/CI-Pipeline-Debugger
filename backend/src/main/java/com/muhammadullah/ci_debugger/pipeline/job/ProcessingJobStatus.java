1871d2ae4b5042dc47bb362a024f3cd726bef630
M.Ullah <muhammadhullah23@gmail.com>
2026-03-17 08:56:02 -0400

    cerate enums for processing job status and type

---
+++ backend/src/main/java/com/muhammadullah/ci_debugger/pipeline/job/ProcessingJobStatus.java
@@ -0,0 +1,8 @@
+package com.muhammadullah.ci_debugger.pipeline.job;
+
+public enum ProcessingJobStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    FAILED
+}
---
+++ backend/src/main/java/com/muhammadullah/ci_debugger/pipeline/job/ProcessingJobType.java
@@ -0,0 +1,5 @@
+package com.muhammadullah.ci_debugger.pipeline.job;
+
+public enum ProcessingJobType {
+    FETCH_STEPS
+}
