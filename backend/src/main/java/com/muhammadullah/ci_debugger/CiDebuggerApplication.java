fe62f5dd0449ebc5c1a2991682af5e23859ff21c
M.Ullah <muhammadhullah23@gmail.com>
2026-03-23 23:58:10 -0400

    disable scheduling for tests

--- backend/src/main/java/com/muhammadullah/ci_debugger/CiDebuggerApplication.java
+++ backend/src/main/java/com/muhammadullah/ci_debugger/CiDebuggerApplication.java
@@ -2,10 +2,12 @@ package com.muhammadullah.ci_debugger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
 public class CiDebuggerApplication {
 
 	public static void main(String[] args) {
