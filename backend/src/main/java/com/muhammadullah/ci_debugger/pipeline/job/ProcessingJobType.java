598465c23c54199a9f0b51ceb74cf5f29048082c
M.Ullah <muhammadhullah23@gmail.com>
2026-04-01 14:54:16 -0400

    add github fetch logs and cluster errors job type

--- backend/src/main/java/com/muhammadullah/ci_debugger/pipeline/job/ProcessingJobType.java
+++ backend/src/main/java/com/muhammadullah/ci_debugger/pipeline/job/ProcessingJobType.java
@@ -1,5 +1,7 @@
 package com.muhammadullah.ci_debugger.pipeline.job;
 
 public enum ProcessingJobType {
-    GITHUB_FETCH_STEPS
+    GITHUB_FETCH_STEPS,
+    GITHUB_FETCH_LOGS,
+    CLUSTER_ERRORS
 }
