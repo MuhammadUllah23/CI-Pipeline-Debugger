8b63d431f9353bf2d0f45fd423b1ff7af6c6041a
M.Ullah <muhammadhullah23@gmail.com>
2026-04-22 23:22:42 -0400

    create findByErrorClusterIdOrderByCreatedAtDesc and findByPipelineRunId

--- backend/src/main/java/com/muhammadullah/ci_debugger/pipeline/error/ErrorOccurrenceRepository.java
+++ backend/src/main/java/com/muhammadullah/ci_debugger/pipeline/error/ErrorOccurrenceRepository.java
@@ -3,6 +3,7 @@ package com.muhammadullah.ci_debugger.pipeline.error;
 import com.muhammadullah.ci_debugger.pipeline.run.PipelineRun;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ErrorOccurrenceRepository extends JpaRepository<ErrorOccurrence, UUID> {
@@ -12,4 +13,15 @@ public interface ErrorOccurrenceRepository extends JpaRepository<ErrorOccurrence
      * for an already-clustered run.
      */
     boolean existsByErrorClusterAndPipelineRun(ErrorCluster errorCluster, PipelineRun pipelineRun);
+
+    /**
+     * Returns all occurrences for a given error cluster, ordered by
+     */
+    List<ErrorOccurrence> findByErrorClusterIdOrderByCreatedAtDesc(UUID errorClusterId);
+
+    /**
+     * Returns all occurrences associated with a given pipeline run.
+     * Used to find which clusters were triggered by a specific run.
+     */
+    List<ErrorOccurrence> findByPipelineRunId(UUID pipelineRunId);
 }
