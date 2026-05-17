8efeea1a449a756320e9f3f2fad03120c9afdd33
M.Ullah <muhammadhullah23@gmail.com>
2026-05-17 13:55:12 -0400

    add local db for tests

--- backend/src/test/java/com/muhammadullah/ci_debugger/CiDebuggerApplicationTests.java
+++ backend/src/test/java/com/muhammadullah/ci_debugger/CiDebuggerApplicationTests.java
@@ -1,15 +1,31 @@
 package com.muhammadullah.ci_debugger;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.junit.jupiter.Container;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 class CiDebuggerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
