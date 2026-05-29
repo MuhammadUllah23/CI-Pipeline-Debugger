21b3878ce132f195dc81c420543e021f5814bcdc
MuhammadUllah23 <78986038+MuhammadUllah23@users.noreply.github.com>
2026-05-29 14:17:58 -0400

    Remove error message from GitHubLogsApiClientTest
    Removed error message from log content in test.

--- backend/src/test/java/com/muhammadullah/ci_debugger/pipeline/run/github/client/GitHubLogsApiClientTest.java
+++ backend/src/test/java/com/muhammadullah/ci_debugger/pipeline/run/github/client/GitHubLogsApiClientTest.java
@@ -57,7 +57,6 @@ class GitHubLogsApiClientTest {
                 2026-04-01T02:31:11.379Z shell: /usr/bin/bash -e {0}
                 2026-04-01T02:31:11.379Z ##[endgroup]
                 2026-04-01T02:31:11.379Z [INFO] Scanning for projects...
-                2026-04-01T02:31:11.379Z ##[error]Process completed with exit code 1.
                 """;
 
         byte[] zipBytes = buildZip(Map.of("0_build.txt", logContent));
