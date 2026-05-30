3f21f1b82204429280ca7a6d818969e8658c450d
MuhammadUllah23 <78986038+MuhammadUllah23@users.noreply.github.com>
2026-05-30 15:03:52 -0400

    Update GitHubLogsApiClientTest.java

--- backend/src/test/java/com/muhammadullah/ci_debugger/pipeline/run/github/client/GitHubLogsApiClientTest.java
+++ backend/src/test/java/com/muhammadullah/ci_debugger/pipeline/run/github/client/GitHubLogsApiClientTest.java
@@ -82,7 +82,6 @@ class GitHubLogsApiClientTest {
                 2026-04-01T02:31:11.379Z shell: /usr/bin/bash -e {0}
                 2026-04-01T02:31:11.379Z [command]/usr/bin/git version
                 2026-04-01T02:31:11.379Z ##[endgroup]
-                2026-04-01T02:31:11.379Z [INFO] Scanning for projects...
                 2026-04-01T02:31:11.379Z ##[error]Process completed with exit code 1.
                 """;
 
